1469055f0d544d6f4e7b7637756b2493ce9db505
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-03-07 22:18:57 +1100

    Updated plot2.R 07/03/2014
    updated plot2.R script to load and subset dataset, generate plot2 and save it as a png file

--- plot2.R
+++ plot2.R
@@ -1,1 +1,22 @@
+## script creates plot2 and saves it as a png file
 
+## Load full dataset
+full_dataset <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+full_dataset$Date <- as.Date(full_dataset$Date, format="%d/%m/%Y")
+
+## Subsetting the dataset
+data <- subset(full_dataset, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(full_dataset)
+
+## Date conversion
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+## Saving as plot2.png file
+dev.copy(png, file="./images/plot2.png", height=500, width=500)
+dev.off()
