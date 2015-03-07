689377f9911e7de5e4d23f9255de77cee0eb892a
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-03-07 22:47:08 +1100

    Updated plot4.R 07/03/2014
    updated plot4.R script to load and subset dataset, generate plot2 and save it as a png file

--- plot4.R
+++ plot4.R
@@ -1,1 +1,35 @@
+## Script creates plot4 and saves it as a png file
 
+## Load full data set
+full_dataset <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+full_dataset$Date <- as.Date(full_dataset$Date, format="%d/%m/%Y")
+
+## Subset data set
+data <- subset(full_dataset, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(full_dataset)
+
+## Date conversion
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data, {
+    plot(Global_active_power~Datetime, type="l", 
+         ylab="Global Active Power (kilowatts)", xlab="")
+    plot(Voltage~Datetime, type="l", 
+         ylab="Voltage (volt)", xlab="")
+    plot(Sub_metering_1~Datetime, type="l", 
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+    plot(Global_reactive_power~Datetime, type="l", 
+         ylab="Global Rective Power (kilowatts)",xlab="")
+})
+
+## Save as plot4.png under the "images" folder
+dev.copy(png, file="./images/plot4.png", height=500, width=600)
+dev.off()
