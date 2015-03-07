4323936fa266c67d8691f4b880a99377c5f7a757
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-03-07 23:13:06 +1100

    Updated plot4.R 07/03/2014

--- plot4.R
+++ plot4.R
@@ -16,20 +16,32 @@ data$Datetime <- as.POSIXct(datetime)
 ## Plot 4
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data, {
-    plot(Global_active_power~Datetime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
-    plot(Voltage~Datetime, type="l", 
-         ylab="Voltage (volt)", xlab="")
-    plot(Sub_metering_1~Datetime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
-    lines(Sub_metering_2~Datetime,col='Red')
-    lines(Sub_metering_3~Datetime,col='Blue')
-    legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-           legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-    plot(Global_reactive_power~Datetime, type="l", 
-         ylab="Global Rective Power (kilowatts)",xlab="")
+
+## Top left
+plot(Global_active_power~Datetime, type="l", 
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+## Top right
+plot(Voltage~Datetime, type="l", 
+     ylab="Voltage (volt)", xlab="")
+
+
+## Bottom left
+plot(Sub_metering_1~Datetime, type="l", 
+     ylab="Global Active Power (kilowatts)", xlab="")
+lines(Sub_metering_2~Datetime,col='Red')
+lines(Sub_metering_3~Datetime,col='Blue')
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Bottom right
+plot(Global_reactive_power~Datetime, type="l", 
+     ylab="Global Rective Power (kilowatts)",xlab="")
 })
 
 ## Save as plot4.png under the "images" folder
 dev.copy(png, file="./images/plot4.png", height=500, width=600)
 dev.off()
+
+## Clear objects from the workspace
+rm(list=ls())
