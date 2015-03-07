c77827b1d401fa160d22f06d5f5aa22b645df832
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-03-07 22:25:34 +1100

    Updated plot1.R 07/03/2014
    updated comments

--- plot1.R
+++ plot1.R
@@ -5,12 +5,12 @@ if (!file.exists("data")) {
 dir.create("data")
 }
 
-## create an "images" directory to save the png files into 
+## Create an "images" directory to save the png files into 
 if (!file.exists("images")) {
 dir.create("images")
 }
 
-## Dowload data set and save it to working data directory
+## Dowload data set and save it to the "data" directory
 ## Source https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl, destfile = "./data/exdata_data_household_power_consumption.zip")
@@ -21,12 +21,12 @@ unzip("./data/exdata_data_household_power_consumption.zip", files = NULL, list =
 junkpaths = FALSE, exdir = "./data", unzip = "internal",
 setTimes = FALSE)
 
-## Loading full dataset
+## Load full data set
 full_dataset <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                          nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 full_dataset$Date <- as.Date(full_dataset$Date, format="%d/%m/%Y")
 
-## Subsetting the dataset
+## Subset data set
 data <- subset(full_dataset, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(full_dataset)
 
@@ -38,6 +38,6 @@ data$Datetime <- as.POSIXct(datetime)
 hist(data$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-## Saving as plot1.png file
+## Save as plot1.png under the "images" folder
 dev.copy(png, file="./images/plot1.png", height=500, width=500)
 dev.off()
