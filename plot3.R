799020a9fed3db2e498d759795cfa8bf53cde4d8
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-03-07 22:32:14 +1100

    Updated plot3.R 07/03/2014
    updated plot3.R script to load and subset dataset, generate plot3 and save it as a png file

--- plot3.R
+++ plot3.R
@@ -1,1 +1,28 @@
+## Script creates plot and saves it as a png file
 
+## Load full data set
+full_dataset <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+full_dataset$Date <- as.Date(full_dataset$Date, format="%d/%m/%Y")
+
+## Subset data set
+data <- subset(full_dataset, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(full_dataset)
+
+## Date conversion
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 3
+with(data, {
+    plot(Sub_metering_1~Datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~Datetime,col='Red')
+    lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Saving as plot3.png file
+dev.copy(png, file="./images/plot3.png", height=500, width=600)
+dev.off()
