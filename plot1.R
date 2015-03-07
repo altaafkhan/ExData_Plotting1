6c59ff287b396dd8ce65e418a6fa88df07f9f324
altaafkhan <altaaf.khan@marketsoft.com.au>
2015-03-07 22:03:30 +1100

    Updated plot1.R 07/03/2014
    updated script to read and subset data set, convert date, generate Plot 1 and save it as a png file to the images folder

--- plot1.R
+++ plot1.R
@@ -20,3 +20,24 @@ dateDownloaded <- date()
 unzip("./data/exdata_data_household_power_consumption.zip", files = NULL, list = FALSE, overwrite = TRUE,
 junkpaths = FALSE, exdir = "./data", unzip = "internal",
 setTimes = FALSE)
+
+## Loading full dataset
+full_dataset <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                         nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+full_dataset$Date <- as.Date(full_dataset$Date, format="%d/%m/%Y")
+
+## Subsetting the dataset
+data <- subset(full_dataset, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(full_dataset)
+
+## Date conversion
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 1
+hist(data$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving as plot1.png file
+dev.copy(png, file="./images/plot1.png", height=500, width=500)
+dev.off()
